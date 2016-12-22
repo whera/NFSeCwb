1161e42bdeb72a148ce46ca404ffce0efe902366
Lucas Coser <lucasccarlos@gmail.com>
2016-12-21 23:36:30 -0200

    Desenvolvimento das configuraçoes de ambiente

---
+++ src/Config/Production/Properties.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Config\Production;
+
+
+class Properties
+{
+    CONST WSDL = 'https://isscuritiba.curitiba.pr.gov.br/Iss.NfseWebService/nfsews.asmx?WSDL';
+    CONST ASMX = 'https://isscuritiba.curitiba.pr.gov.br/Iss.NfseWebService/nfsews.asmx';
+    CONST CERTIFICATE = '/path to .pem';
+    CONST NFSE_IMAGE_URL = 'http://isscuritiba.curitiba.pr.gov.br/portalNfse/ImagemNfse.axd?doc=%s&num=%s&cod=%s';
+}
